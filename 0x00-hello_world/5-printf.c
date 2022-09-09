12f3fbdc2019a30b29e25a5402ec6f8db7d6b8fc
ejiroogbeifo <ejiroogbeifo@gmail.com>
2022-09-09 13:32:36 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+
+
+/**
+
+ * main - Entry point
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
+
+int main(void)
+
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
+}
+On branch master
+Your branch is up to date with 'origin/master'.
+
+Changes not staged for commit:
+  (use "git add <file>..." to update what will be committed)
+  (use "git restore <file>..." to discard changes in working directory)
+	modified:   README.md
+
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+	../0-preprocessor
+	5-printf.c
+	../README.md
+
+no changes added to commit (use "git add" and/or "git commit -a")
