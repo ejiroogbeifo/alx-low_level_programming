299d27b6d3af2cbeb0f459f6354aafac61546476
ejiroogbeifo <ejiroogbeifo@gmail.com>
2022-09-09 13:37:19 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,13 +4,17 @@
 
 /**
 
- * main - Entry point
+* main - print the string in the put function
 
- *
+*
 
- * Return: Always 0 (Success)
+* Description: using the main function
 
- */
+* this program prints "Programming is like building a multilingual puzzle
+
+* Return: 0
+
+*/
 
 int main(void)
 
@@ -21,21 +25,3 @@ int main(void)
 	return (0);
 
 }
-On branch master
-Your branch is up to date with 'origin/master'.
-
-Changes not staged for commit:
-  (use "git add <file>..." to update what will be committed)
-  (use "git restore <file>..." to discard changes in working directory)
-	modified:   README.md
-
-Untracked files:
-  (use "git add <file>..." to include in what will be committed)
-	../0-preprocessor
-	5-printf.c
-	../README.md
-
-no changes added to commit (use "git add" and/or "git commit -a")
-[master 12f3fbd] Hello, printf
- 2 files changed, 39 insertions(+)
- create mode 100755 0x00-hello_world/5-printf.c
