a2d141ca7a3374474384692368048b453c5369d0
ejiroogbeifo <ejiroogbeifo@gmail.com>
2022-09-11 03:46:33 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,79 @@
+Task 6.txt
+
+Who has access
+
+A
+
+System properties
+
+Type
+
+Text
+
+Size
+
+325 bytes
+
+Storage used
+
+325 bytes
+
+Location
+
+Variables if else while
+
+Owner
+
+Antony Bahati
+
+Modified
+
+Sep 10, 2022 by Antony Bahati
+
+Opened
+
+11:46 AM by me
+
+Created
+
+Sep 10, 2022
+
+No description
+
+Viewers can download
+
+#include <stdio.h>
+
+/**
+
+ * main - print if the number is postive, zero, or negative
+
+ *
+
+ * Description: using the main function
+
+ * this program prints "Programming is positive, zero, or negative
+
+ * Return: 0
+
+ */
+
+int main(void)
+
+{
+
+int n;
+
+for (n = '0'; n <= '9'; n++)
+
+{
+
+	putchar(n);
+
+}
+
+putchar('\n');
+
+return (0);
+
+}
