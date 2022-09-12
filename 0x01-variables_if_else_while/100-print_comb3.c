645204d7d91549573c631e23859150c5e7b926d1
ejiroogbeifo <112100980+ejiroogbeifo@users.noreply.github.com>
2022-09-12 19:08:58 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,45 +1,26 @@
 #include <stdio.h>
 
 /**
-
  * main - program that prints all possible different combinations of 2 digits
-
  * Return: 0
-
  */
-
 int main(void)
-
 {
-
 	int c = 0;
-
 	int f_d;
-
 	int l_d;
-
-
-
 	while (c <= 99)
 
 	{
-
 		f_d = (c / 10 + '0');
 
 		l_d = (c % 10 + '0');
-
-
-
 		if (f_d < l_d)
 
 		{
-
 			putchar(f_d);
 
 			putchar(l_d);
-
-
-
 			if (c != 89)
 
 			{
@@ -47,17 +28,12 @@ int main(void)
 				putchar(',');
 
 				putchar(' ');
-
 			}
-
 		}
 
 		c++;
-
 	}
 
 putchar('\n');
-
 return (0);
-
 }
