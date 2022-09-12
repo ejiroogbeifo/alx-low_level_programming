043597e8ee442f4733195f700d2647483d51f1e9
ejiroogbeifo <112100980+ejiroogbeifo@users.noreply.github.com>
2022-09-12 19:03:28 +0100

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,61 +1,11 @@
-Task 6.txt
-
-Who has access
-
-A
-
-System properties
-
-Type
-
-Text
-
-Size
-
-325 bytes
-
-Storage used
-
-325 bytes
-
-Location
-
-Variables if else while
-
-Owner
-
-Antony Bahati
-
-Modified
-
-Sep 10, 2022 by Antony Bahati
-
-Opened
-
-11:46 AM by me
-
-Created
-
-Sep 10, 2022
-
-No description
-
-Viewers can download
-
 #include <stdio.h>
 
 /**
-
  * main - print if the number is postive, zero, or negative
-
  *
-
  * Description: using the main function
-
  * this program prints "Programming is positive, zero, or negative
-
  * Return: 0
-
  */
 
 int main(void)
