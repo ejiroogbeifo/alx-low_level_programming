49277974880794db9524ee7b7e8b42b95db906fa
ejiroogbeifo <112100980+ejiroogbeifo@users.noreply.github.com>
2022-09-26 09:34:43 +0100

    Update and rename MAIN.H to main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,11 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <stdio.h>
 /**
-* void prototypes()
-* int prototypes()
-*/
-
+ *void prototypes()
+ *int prototypes()
+ *char prototypes()
+ */
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
